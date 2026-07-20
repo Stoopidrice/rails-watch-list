e008c341640d7ffa61f4994c1618b707be5afa97
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-07-20 16:26:30 +0900

    done

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -9,8 +9,27 @@ window.Stimulus   = application
 export { application }
 
 const meow = new Audio("/assets/meow.mp3");
+document.addEventListener("turbo:load", () => {
+  document.addEventListener("click", (event) => {
+    if (event.target.closest(".delete-link")) {
+      return;
+    }
+// target closest checks the parent element and rules it out as an exception
+// so in this case, play on click unless it has the class delete link
+    meow.currentTime = 0;
+    meow.play();
+  });
+});
 
-document.addEventListener("click", () => {
-  meow.currentTime = 0;
-  meow.play();
+
+
+const angry = new Audio("/assets/angry.mp3");
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll(".delete-link").forEach((link) => {
+
+    link.addEventListener("click", () => {
+      angry.currentTime = 0;
+      angry.play();
+    });
+  });
 });
